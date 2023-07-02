232245392bb5091464117a895d07b61a0e6db6bc
abijith-tk <abijithtk1212@gmail.com>
2023-07-02 13:03:39 +0530

    task-7

--- task-7-src/task-7.js
+++ task-7-src/task-7.js
@@ -1,66 +1,162 @@
 function addStudent(){
-    let name = document.getElementById('name').value;
-    let subject = document.getElementById('subject').value;
-    let std_id = document.getElementById('std_id').value;
-    let mark1 = Number(document.getElementById('mark1').value);
-    let mark2 = Number(document.getElementById('mark2').value);
-    let external = Number(document.getElementById('external').value);
-    let total = mark1 + mark2 + external;
+    let name=document.getElementById('name').value;
+    let regID=document.getElementById('regID').value;
     
-// creating student details object
-    let studentDetails = {
+    let subject1=document.getElementById("subject1").value;
+    let internal1=Number(document.getElementById('internal1').value);
+    let external1=Number(document.getElementById('external1').value);
+    let total1=internal1+external1;
+
+    let subject2=document.getElementById("subject2").value;
+    let internal2=Number(document.getElementById('internal2').value);
+    let external2=Number(document.getElementById('external2').value);
+    let total2=internal2+external2;
+
+    let subject3=document.getElementById("subject3").value;
+    let internal3=Number(document.getElementById("internal3").value);
+    let external3=Number(document.getElementById("external3").value);
+    let total3=internal3+external3;
+
+    let subject4=document.getElementById("subject4").value;
+    let internal4=Number(document.getElementById("internal4").value);
+    let external4=Number(document.getElementById("external4").value);
+    let total4=internal4+external4;
+
+    let subject5=document.getElementById("subject5").value;
+    let internal5=Number(document.getElementById("internal5").value);
+    let external5=Number(document.getElementById("external5").value);
+    let total5=internal5+external5;
+
+    let gpa=(total1+total2+total3+total4+total5)/50;
+
+    let studentDetails={
         name : name,
-        subject : subject,
-        std_id : std_id,
-        mark1 : Number(mark1),
-        mark2 : Number(mark2),
-        external : Number(external),
-        total : total
+        regID : regID,
+        subject1 : subject1,
+        internal1 : internal1,
+        external1 : external1,
+        total1 : total1,
+        subject2 : subject2,
+        internal2 : internal2,
+        external2 : external2,
+        total2 : total2,
+        subject3 : subject3,
+        internal3 : internal3,
+        external3 : external3,
+        total3 : total3,
+        subject4 : subject4,
+        internal4 : internal4,
+        external4 : external4,
+        total4 : total4,
+        subject5 : subject5,
+        internal5 : internal5,
+        external5 : external5,
+        total5 : total5,
+        gpa : gpa
     }
-    
-    if(localStorage.getItem("Student-marklist")===null){
-        let marklist_array = []
+
+    console.log(studentDetails);
+
+    if(localStorage.getItem("Student-marklist")==null){
+        let marklist_array = [];
         marklist_array.push(studentDetails);
-        console.log(marklist_array)
         localStorage.setItem("Student-marklist",JSON.stringify(marklist_array));
     }
     else{
-        let marklist_array = JSON.parse(localStorage.getItem("Student-marklist"))
-        marklist_array.push(studentDetails)
-        console.log(marklist_array)
-        localStorage.setItem("Student-marklist",JSON.stringify(marklist_array));
+        let marklist_array=JSON.parse(localStorage.getItem("Student-marklist"));
+        marklist_array.push(studentDetails);
+        localStorage.setItem("Student-marklist",JSON.stringify(marklist_array));    
     }
 
     displayStudent()
 
 }
 
-let students = JSON.parse(localStorage.getItem("Student-marklist")) || []
-
 function displayStudent(){
-    let students=JSON.parse(localStorage.getItem("Student-marklist"));
-    console.log(students)
-    let results = document.getElementById("student-list")
-    if(students===null){
-        results.innerHTML='<h5 class="text-center fw-normal">No students added yet</h5>'
+    let student=JSON.parse(localStorage.getItem("Student-marklist"));
+    console.log(student);
+    let results = document.getElementById("student-list");
+    if(student===null){
+        results.innerHTML=`<h5 class="text-center fw-normal">No students added yet</h5>`
     }
     else{
-        results.innerHTML=`
-        <table>
-            <tr>Name</tr>
-            <tr>Reg No</tr>
-            <tr> </tr>
-        </table>
-        `
-        students.forEach(element => {
-            
+        student.forEach(element => {
             results.innerHTML+=`
-            <tr>
-            <td>${element.name}</td>
-            <td>${element.std_id}</td>
-            <td><button class="btn">View marks</td>
-            </tr>
+            <div class="row">
+                <div class="col">
+                    ${element.name}
+                </div>
+                <div class="col">
+                    ${element.regID}
+                </div>
+                <div class="col">
+                    <a href="task-7-mark.html?val=${element.regID}">
+                        <button type="button" id="btn" value="${element.regID}" class="btn btn-secondary btn-sm">View Marks</button>
+                    </a>
+                </div>
+                <br><br> 
+            </div>
             `
         });
+
     }
+}
+
+
+function display(){
+
+
+    let selectedID = window.location.search.substring(1).split("&")[0].split("=")[1]
+    let student = JSON.parse(localStorage.getItem("Student-marklist"));
+    // console.log(student);
+    let results = document.getElementById("student-marks");
+    // console.log(student)
+
+    // Finding the object with particular id
+    let obj = student.find( x => x.regID === selectedID)
+    results.innerHTML=`
+    <tr class="text-center">
+        <td>${obj.subject1}</td>
+        <td>${obj.internal1}</td>
+        <td>${obj.external1}</td>
+        <td>${obj.total1}</td>
+    </tr>
+
+    <tr class="text-center">
+        <td>${obj.subject2}</td>
+        <td>${obj.internal2}</td>
+        <td>${obj.external2}</td>
+        <td>${obj.total2}</td>
+    </tr>
+
+    <tr class="text-center">
+        <td>${obj.subject3}</td>
+        <td>${obj.internal3}</td>
+        <td>${obj.external3}</td>
+        <td>${obj.total3}</td>
+    </tr>
+
+    <tr class="text-center">
+        <td>${obj.subject4}</td>
+        <td>${obj.internal4}</td>
+        <td>${obj.external4}</td>
+        <td>${obj.total4}</td>
+    </tr>
+
+    <tr class="text-center">
+        <td>${obj.subject5}</td>
+        <td>${obj.internal5}</td>
+        <td>${obj.external5}</td>
+        <td>${obj.total5}</td>
+    </tr>
+    <tr class="text-center">
+    <th> 
+    <h6 class="fw-bold">GPA </h4>
+    </th>
+    <th></th><th></th>
+    <th>
+    <h6>${obj.gpa}</h4>
+    </th>
+    </tr>
+    `
 }
